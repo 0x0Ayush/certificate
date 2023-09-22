1b1d83ffe2a5031a1d54f7c222c8b202ca739092
keyurpatel29 <keyurpatel8401@gmail.com>
2023-09-22 11:33:45 +0530

    home and navbar

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { ConnectWallet } from "@thirdweb-dev/react";
+import styled from 'styled-components';
 import "./Navbar.css";
 import logooo from'../images/logooo.gif';
+import profile from '../images/profile.png';
 // import Home from "../Pages/Home/Home";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHouseChimney, faAward , faIdCard, faCircleInfo, faLightbulb} from "@fortawesome/free-solid-svg-icons";
@@ -10,13 +12,29 @@ import { Link } from "react-router-dom";
 
 
 const Navbar = () => {
+
+  // const [hover, setHover] = useState(false);
+
+  const buttonStyle = {
+    background: '#BA5E00',
+    color: 'white',
+    width: '100px',
+    border: 'none',
+    padding: '15px 10px',
+    borderRadius: '5px',
+    cursor: 'pointer',
+    
+  };
+
+  
   return (
+    
     <div className="Main-header">
       <div className="header">
         <div className='logo'> 
           <a href="/home">
             <img src={logooo} type="video/gif/" autoPlay loop muted width={230} 
-              style={{background: "transparent"}}
+             
             /> 
           </a>
 
@@ -26,39 +44,53 @@ const Navbar = () => {
 
           <div className="Home">
             <a href="/home">
-              <i><FontAwesomeIcon icon={faHouseChimney}  /></i> Home
+              <i><FontAwesomeIcon icon={faHouseChimney} className="Home-icon"   /></i> Home
             </a>
           </div>
 
           <div className="Get-document">
             <a href="/getdocument">
-              <i><FontAwesomeIcon icon={faIdCard} /></i>Get Document
+              <i><FontAwesomeIcon icon={faIdCard} className="Get-icon"/></i> Get Document
               </a>
           </div>
 
           <div className="Issued-document">
             <a href="/issueddocument">
-              <i><FontAwesomeIcon icon={faAward} /></i>Issued Document
+              <i><FontAwesomeIcon icon={faAward} className="Issued-icon"/></i> Issued Document
             </a>
           </div>
 
           <div className="About">
             <a href="/About">
-              <i><FontAwesomeIcon icon={faCircleInfo} /></i>About</a>
+              <i><FontAwesomeIcon icon={faCircleInfo} className="About-icon"/></i> About</a>
           </div>
 
           <div className="Help">
             <a href="/help">
-              <i><FontAwesomeIcon icon={faLightbulb} /></i>Help</a>
+              <i><FontAwesomeIcon icon={faLightbulb} className="Help-icon"/></i> Help</a>
           </div>
         </nav>
 
-        <ConnectWallet 
-          dropdownPosition={{ 
-             side: "right",
-             align: "center",
-             }}
-        />
+        <ConnectWallet style={buttonStyle}/>
+
+        {/* <div className="hero">
+          <img src={profile} className="user-pic" ></img>
+
+          <div className="sub-menu-wrap">
+            <div className="sub-menu">
+              <div className="user-info">
+                <img src={profile} ></img>
+                <h2>keyur patel</h2>
+              </div>
+              <hr></hr>
+
+              <a href="#" className=" sub-menu-link">
+                <p>Info</p>
+              
+              </a>
+            </div>
+          </div> */}
+        {/* </div> */}
       </div> 
     </div>
   );
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,45 +1,51 @@
 import React from 'react'
 import "./Home.css"
+import registrationform from'../../images/registrationform.png';
 //import safehaven from '../../images/safehaven.mp4'//
 
 const Home = () => {
   return (
-    <div className='homepage'>
-      <div>Home</div>
-      <div>Home</div>
-      <div>Home</div>
-      <div>Home</div>
-      <div>Home</div>
+    <div className='a1'>
 
-      <div>Home</div>
+      <div className='registration-img'>
+        <img src={registrationform} ></img>
+      </div>
 
-      <div>Home</div>
-      <div>Home</div>
-      <div>Home</div>
-      <div>Home</div>
-      <div>Home</div>
-      <div>Home</div>
-      <div>Home</div>
-      <div>Home</div>
-      <div>Home</div>
-      <div>Home</div>
-      <div>Home</div>
-      <div>Home</div>
-      <div>Home</div>
-      <div>Home</div>
-      <div>Home</div>
-      <div>Home</div><div>Home</div>
-      
+      <div className='main-containerr'>
+        <div className='containerr'>
+          <div className='imagee'>
+            <div className='form-box'>
+              <div className='form'>
+                <h2>Register</h2>
+                <div className='input-box'>
+                  <input type='text' required></input>
+                  <label for=''>Username</label>
+                </div>
 
-      <div>Home</div>
+                <div className='input-box'>
+                  <input type='text' required></input>
+                  <label for=''>Gmail</label>
+                </div>
 
-      <div>Home</div>
+                <div className='input-box'>
+                  <input type='text' required></input>
+                  <label for=''>Mobile No.</label>
+                </div>
 
+                <div className='input-box'>
+                  <input type='text' required></input>
+                  <label for=''>Age</label>
+                </div>
 
-        <div>
+                <div className='register-button'>
+                 <button className='text'>Register</button>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
+      </div>
     </div>
-    
   )
 }
 
